c53b8b877065ceb291ee6d4d622074f56eebac79
Frederic <nouiz@nouiz.org>
2010-06-21 12:56:15 -0400

    add info about version of software.

--- numpy/run.sh
+++ numpy/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+cat /proc/cpuinfo |grep "model name"|uniq > ${HOSTNAME}_config.conf
+free >> ${HOSTNAME}_config.conf
+uname -a >>  ${HOSTNAME}_config.conf
+python -c 'import numpy;print "Numpy version:",numpy.__version__' >>  ${HOSTNAME}_config.conf
+python -c 'import numpy;print "Numpy config";numpy.__config__.show()' >>  ${HOSTNAME}_config.conf
+python -V >>  ${HOSTNAME}_config.conf
+
 python mlp.py 784 500 10 1 1000 > ${HOSTNAME}_mlp_1.bmark
 python mlp.py 784 500 10 60 100 > ${HOSTNAME}_mlp_60.bmark
 
